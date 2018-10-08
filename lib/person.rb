4e1584098be1bb33c8050b9d863af9a602b558b6
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-08 19:22:44 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(name)
+    dog_name = name
+  end
+  def name
+    puts name
+  end
+  def breed=(breed)
+    dog_breed = breed
+  end
+  def breed
+    puts dog_breed
+  end
+end
